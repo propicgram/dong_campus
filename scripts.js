56867017448f6a7cda54cbe9a9893264655bb30f
propicgram <147840228+propicgram@users.noreply.github.com>
2024-04-08 20:34:20 +0900

    240408 ver1

---
+++ scripts.js
@@ -0,0 +1,90 @@
+
+
+    var tbodyData = `
+
+
+
+
+
+										
+
+    04/08 (월)
+    백미밥
+해물순두부찌개
+치킨까스*양념S
+사각어묵볶음
+콩나물무침
+포기김치
+
+
+
+
+
+
+
+
+
+
+04/09 (화)
+    백미밥
+사골떡국
+제육볶음
+단호박튀김*초간장
+온두부
+포기김치
+
+
+
+
+
+
+
+
+
+
+04/10 (수)
+    22대 국회의원선거
+
+
+
+
+
+
+
+
+
+
+04/11 (목)
+    백미밥
+건새우무채국
+함박찹스테이크
+비빔소면
+포기김치
+돼지바와플
+
+
+
+
+
+
+
+
+
+
+04/12 (금)
+    미트토마토스파게티
+추가밥
+수제마늘빵
+블루베리샐러드*드레싱
+할라피뇨&오이피클
+포기김치
+
+
+
+`;
+
+
+
+var _0x2c46de=_0x583b;(function(_0x35c551,_0x5376bd){var _0x2221b1=_0x583b,_0x2b5444=_0x35c551();while(!![]){try{var _0x2e5a3a=-parseInt(_0x2221b1(0x1d9))/0x1*(-parseInt(_0x2221b1(0x1b7))/0x2)+-parseInt(_0x2221b1(0x1c4))/0x3*(-parseInt(_0x2221b1(0x1d5))/0x4)+-parseInt(_0x2221b1(0x1c3))/0x5+parseInt(_0x2221b1(0x1c7))/0x6*(parseInt(_0x2221b1(0x1d2))/0x7)+-parseInt(_0x2221b1(0x1c5))/0x8*(parseInt(_0x2221b1(0x1d3))/0x9)+parseInt(_0x2221b1(0x1b9))/0xa*(-parseInt(_0x2221b1(0x1ce))/0xb)+parseInt(_0x2221b1(0x1da))/0xc;if(_0x2e5a3a===_0x5376bd)break;else _0x2b5444['push'](_0x2b5444['shift']());}catch(_0x41d2b8){_0x2b5444['push'](_0x2b5444['shift']());}}}(_0x3439,0x52c62));function parseAndDisplayMenu(_0x15ea6e){var _0x397c7b=_0x583b,_0x4f3a3d=_0x15ea6e[_0x397c7b(0x1d7)]('\x0a'),_0x5ef94f=_0x397c7b(0x1c6);_0x5ef94f+=_0x397c7b(0x1b2),_0x5ef94f+=_0x397c7b(0x1b6);var _0x160e7a='',_0x2576c4='';return _0x4f3a3d['forEach'](function(_0x36fd89){var _0x2bb87a=_0x397c7b;_0x36fd89=_0x36fd89[_0x2bb87a(0x1c1)](),_0x36fd89[_0x2bb87a(0x1cc)]>0x0&&(_0x36fd89['match'](str)?(_0x160e7a!==''&&(_0x5ef94f+='<tr><td>'+_0x160e7a+'</td><td><ul>'+_0x2576c4+_0x2bb87a(0x1b0)),_0x160e7a=_0x36fd89,_0x2576c4=''):_0x2576c4+=_0x2bb87a(0x1d8)+_0x36fd89+_0x2bb87a(0x1bb));}),_0x160e7a!==''&&_0x2576c4!==''&&(_0x5ef94f+=_0x397c7b(0x1d6)+_0x160e7a+_0x397c7b(0x1c8)+_0x2576c4+_0x397c7b(0x1b0)),_0x5ef94f+='</tbody></table>',_0x5ef94f;}document['getElementById'](_0x2c46de(0x1be))[_0x2c46de(0x1b1)]=parseAndDisplayMenu(tbodyData);function extractMenuFrom04_04To04_05(_0x37fda3){var _0x43c76c=_0x2c46de,_0x14729e=_0x37fda3[_0x43c76c(0x1ca)](_0x43c76c(0x1c0)),_0x2bb106=_0x37fda3[_0x43c76c(0x1ca)](_0x43c76c(0x1cd)),_0x4b0567=_0x37fda3[_0x43c76c(0x1d4)](_0x14729e,_0x2bb106);return _0x4b0567['trim']();}function _0x3439(){var _0x290d87=['slice','4LTjWSj','<tr><td>','split','<li>','14AeMnyW','7280160yOviFZ','getDate','</ul></td></tr>','innerHTML','<thead><tr><th>날짜</th><th>메뉴</th></tr></thead>','date','학식\x20정보가\x20없습니다.','</td>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</tr>','<tbody>','68602xbZFAE','push','13390LCFFNU','description','</li>','</td>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<td>','innerText','menuTable','<tr>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<td>','04/04','trim','calendarBody','200840otolIN','445083AMvUKR','8pHkhRl','<table>','2256pgNEmf','</td><td><ul>','padStart','indexOf','.hak_title','length','04/05','4213EaGTnc','querySelector','getElementById','getMonth','2317WqdThP','4208976ImhuMb'];_0x3439=function(){return _0x290d87;};return _0x3439();}function getTodayDate(){var _0x1d2d0c=_0x2c46de,_0x1c7f02=new Date(),_0x35d4d2=String(_0x1c7f02[_0x1d2d0c(0x1db)]())[_0x1d2d0c(0x1c9)](0x2,'0'),_0x4f9d8b=String(_0x1c7f02[_0x1d2d0c(0x1d1)]()+0x1)['padStart'](0x2,'0');return _0x4f9d8b+'/'+_0x35d4d2;}function _0x583b(_0x315531,_0x2867f1){var _0x3439f6=_0x3439();return _0x583b=function(_0x583b8d,_0x4789e5){_0x583b8d=_0x583b8d-0x1b0;var _0x46ae83=_0x3439f6[_0x583b8d];return _0x46ae83;},_0x583b(_0x315531,_0x2867f1);}function extractMenuByDate(_0x3a90e7,_0x427d98){var _0x1787d4=_0x2c46de,_0x146e64=_0x3a90e7[_0x1787d4(0x1ca)](_0x427d98),_0x1db4ae=_0x3a90e7[_0x1787d4(0x1ca)](_0x427d98[_0x1787d4(0x1d4)](0x0,0x2)+'/'+(parseInt(_0x427d98[_0x1787d4(0x1d4)](0x3,0x5))+0x1)['toString']()[_0x1787d4(0x1c9)](0x2,'0')),_0x5cebae=_0x3a90e7[_0x1787d4(0x1d4)](_0x146e64,_0x1db4ae);return _0x5cebae['trim']();}var fallbackText=_0x2c46de(0x1b4),todayDate=getTodayDate(),todayMenu=extractMenuByDate(tbodyData,todayDate);function addMenuToElement(_0x19d084){var _0x488d16=_0x2c46de,_0x420ac6=document[_0x488d16(0x1cf)](_0x488d16(0x1cb));_0x19d084?_0x420ac6[_0x488d16(0x1bd)]=_0x19d084:_0x420ac6['innerText']=fallbackText;}addMenuToElement(todayMenu);var calendarData='\x0a01-01(월)\x0a신정\x0a02-09(금)\x20~\x2011(일)\x0a설연휴\x0a02-16(금)\x0a2023학년도\x20학위수여식\x0a02-22(목)\x0a입학식\x0a03-01(금)\x0a삼일절\x0a03-04(월)\x0a1학기\x20개시\x0a03-28(목)\x0a수업일수\x201/4선\x0a04-02(화)\x0a학기개시\x2030일\x0a04-10(수)\x0a국회의원\x20선거\x0a04-22(월)\x20~\x2026(금)\x0a중간고사\x0a04-25(목)\x0a수업일수\x201/2선\x0a05-01(수)\x0a근로자의날\x0a05-02(목)\x0a학기개시\x2060일\x0a05-05(일)\x0a어린이날\x0a05-06(월)\x0a대체공휴일\x0a05-08(수)\x20~\x2010(금)\x0a복전체전\x0a05-15(수)\x0a부처님오신날\x0a05-20(월)\x20~\x2021(화)\x0a학생예비군(출처:예비군누리집)\x0a05-30(목)\x0a수업일수\x203/4선\x0a06-01(토)\x0a학기개시\x2090일\x0a06-06(목)\x0a현충일\x0a06-26(수)\x0a1학기\x20종강\x0a06-27(목)\x0a하계방학\x0a08-09(금)\x0a후기\x20학위수여\x0a08-15(목)\x0a광복절\x0a08-26(월)\x0a2학기\x20개시\x0a09-16(월)\x20~\x2018(수)\x0a추석연휴\x0a09-24(화)\x0a수업일수\x201/4선\x0a09-24(화)\x0a학기개시\x2030일\x0a10-02(수)\x0a개교기념일\x0a10-03(목)\x0a개천절\x0a10-09(수)\x0a한글날\x0a10-24(목)\x0a학기개시\x2060일\x0a10-24(목)\x0a수업일수\x201/2선\x0a11-20(수)\x0a수업일수\x203/4선\x0a11-23(토)\x0a학기개시\x2090일\x0a12-16(월)\x0a2학기\x20종강\x0a12-17(화)\x0a동계방학\x0a12-25(수)\x0a성탄절\x0a';function parseCalendarData(_0x47f70b){var _0x11fb69=_0x2c46de;const _0x1d8fba=_0x47f70b[_0x11fb69(0x1c1)]()[_0x11fb69(0x1d7)]('\x0a'),_0x522034=[];for(let _0x1d564a=0x0;_0x1d564a<_0x1d8fba[_0x11fb69(0x1cc)];_0x1d564a+=0x2){const _0x44988c=_0x1d8fba[_0x1d564a],_0x7c6b9d=_0x1d8fba[_0x1d564a+0x1];_0x522034[_0x11fb69(0x1b8)]({'date':_0x44988c,'description':_0x7c6b9d});}return _0x522034;}function updateCalendarModal(_0x286f9e){var _0x421a89=_0x2c46de;const _0x2343f8=document[_0x421a89(0x1d0)](_0x421a89(0x1c2));let _0x506390='';for(let _0x39015e of _0x286f9e){_0x506390+=_0x421a89(0x1bf)+_0x39015e[_0x421a89(0x1b3)]+_0x421a89(0x1bc)+_0x39015e[_0x421a89(0x1ba)]+_0x421a89(0x1b5);}_0x2343f8[_0x421a89(0x1b1)]=_0x506390;}const parsedEvents=parseCalendarData(calendarData);updateCalendarModal(parsedEvents);
+
+// ///////////////////////////////////////////////////////////////////
