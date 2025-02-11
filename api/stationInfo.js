54206290486d9d60a47f94ac5d02584bade0d587
propicgram <147840228+propicgram@users.noreply.github.com>
2025-02-12 02:27:07 +0900

    Update stationInfo.js

--- api/stationInfo.js
+++ api/stationInfo.js
@@ -1,35 +1,17 @@
-let cacheData = null;
-let cacheTimestamp = 0;
+const fetch = require('node-fetch');
 
 module.exports = async function handler(req, res) {
-    const now = Date.now();
-    const forceUpdate = req.query.forceUpdate === 'true';  // 강제 업데이트 파라미터 확인
-
-    // 캐시 무효화 조건
-    if (!forceUpdate && cacheData && now - cacheTimestamp < 30000) {
-        console.log('Serving cached data');
-        res.setHeader('Cache-Control', 'public, max-age=30');
-        return res.status(200).send(cacheData);
-    }
-
     try {
         const apiUrl = 'http://ws.bus.go.kr/api/rest/stationinfo/getStationByUid';
         const serviceKey = process.env.SEOUL_BUS_API_KEY;
         const arsId = req.query.arsId;
 
-        // 외부 API 호출
-        console.log('Fetching new data from external API');
         const response = await fetch(`${apiUrl}?serviceKey=${encodeURIComponent(serviceKey)}&arsId=${arsId}`);
         if (!response.ok) throw new Error('Failed to fetch data from external API');
 
         const data = await response.text();
 
-        // 최신 데이터를 캐시에 저장
-        cacheData = data;
-        cacheTimestamp = now;
-
-        // 클라이언트 캐시 설정 (30초 캐시)
-        res.setHeader('Cache-Control', 'public, max-age=30');
+        // CORS 허용 헤더 설정
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.status(200).send(data);
 
